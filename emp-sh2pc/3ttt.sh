ccf5e9496632bd2c0bda0444aa0f9583bbba4bd6
Yanping <yz488@duke.edu>
2022-03-21 15:17:55 +0000

    results

--- emp-sh2pc/3ttt.sh
+++ emp-sh2pc/3ttt.sh
@@ -1,3 +1,4 @@
+:'
 for i in {1..5}
 do
    echo "Welcome $i times"
@@ -16,7 +17,8 @@ do
     ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 0.1 100 2 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 0.1 100 2 1
     ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 0.1 100 0 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 0.1 100 0 1
 done
-:'
+'
+
 for i in {1..5}
 do
    echo "Welcome $i times"
@@ -29,14 +31,15 @@ do
     ./bin/test_leaf_all_run 1 12345 taxi_ss1.txt 100 1 1000 1 &./bin/test_leaf_all_run 2 12345 taxi_ss2.txt 100 1 1000 1
     ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 1 1000 2 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 1 1000 2 1
     ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 1 1000 0 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 1 1000 0 1
-
-    #T:100; eps:0.1; N:1000
-    ./bin/test_leaf_all_run 1 12345 taxi_ss1.txt 100 0.1 1000 1 &./bin/test_leaf_all_run 2 12345 taxi_ss2.txt 100 0.1 1000 1
-    ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 0.1 1000 2 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 0.1 1000 2 1
-    ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 0.1 1000 0 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 0.1 1000 0 1
 done
 
+#    #T:100; eps:0.1; N:1000
+#    ./bin/test_leaf_all_run 1 12345 taxi_ss1.txt 100 0.1 1000 1 &./bin/test_leaf_all_run 2 12345 taxi_ss2.txt 100 0.1 1000 1
+#    ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 0.1 1000 2 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 0.1 1000 2 1
+ #   ./bin/test_tree_all_run 1 12345 taxi_ss1.txt 100 0.1 1000 0 1 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 100 0.1 1000 0 1
+#done
 
+:'
 for i in {1..5}
 do
    echo "Welcome $i times"
