d04e008951e63db65f273d5818996d9b686630fb
Yanping <yz488@duke.edu>
2022-08-02 23:01:03 +0000

    run100 100

---
+++ emp-sh2pc/tmprun3.sh
@@ -0,0 +1,17 @@
+start_time=$(date +%s)
+./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 100 1 100 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss1.txt 100 1 100 2 1
+end_time1=$(date +%s)
+elapsed1=$(( end_time1 - start_time ))
+echo $elapsed1
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 100 1 100 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 100 1 100 2 0
+end_time2=$(date +%s)
+elapsed2=$(( end_time2 - start_time ))
+echo $elapsed2
+./bin/test_leaf_baseline_run 1 12345 bin40_ss1.txt 100 1 100 2 1 &./bin/test_leaf_baseline_run 2 12345 bin40_ss2.txt 100 1 100 2 1
+end_time3=$(date +%s)
+elapsed3=$(( end_time3 - start_time ))
+echo $elapsed3
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 100 1 100 0 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 100 1 100 0 0
+end_time4=$(date +%s)
+elapsed4=$(( end_time4 - start_time ))
+echo $elapsed4
