d7ea3fbcc9aae3ea8048f8ad40f6b8d701ead4d1
Yanping <yz488@duke.edu>
2022-08-18 17:42:04 +0000

    result

--- emp-sh2pc/tmprun5.sh
+++ emp-sh2pc/tmprun5.sh
@@ -1,9 +1,9 @@
 start_time=$(date +%s)
-./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss2.txt 200 1 1000 2 1
+./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 200 0.1 1000 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss2.txt 200 0.1 1000 2 1
 end_time1=$(date +%s)
 elapsed1=$(( end_time1 - start_time ))
 echo $elapsed1
-./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 0 &./bin/test_tree_all_run 2 12345/ bin40_ss2.txt 200 1 1000 2 0
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 0.1 1000 2 0 &./bin/test_tree_all_run 2 12345/ bin40_ss2.txt 200 0.1 1000 2 0
 end_time2=$(date +%s)
 elapsed2=$(( end_time2 - start_time ))
 echo $elapsed2
@@ -11,7 +11,7 @@ echo $elapsed2
 #end_time3=$(date +%s)
 #elapsed3=$(( end_time3 - start_time ))
 #echo $elapsed3
-./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 0 0 &./bin/test_tree_all_run 2 12345/ bin40_ss2.txt 200 1 1000 0 0
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 0.1 1000 0 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 200 0.1 1000 0 0
 end_time4=$(date +%s)
 elapsed4=$(( end_time4 - start_time ))
 echo $elapsed4
