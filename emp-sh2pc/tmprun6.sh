147291ada6b06f782002005b169501adf1224096
ypzhang725 <13843165929@163.com>
2022-08-20 00:50:56 -0400

    run new

---
+++ emp-sh2pc/tmprun6.sh
@@ -0,0 +1,22 @@
+# N = 1000; T = 200; eps = 1; leaf  
+./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss2.txt 200 1 1000 2 1
+
+# N = 1000; T = 200; eps = 1; tree_naive 
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 0 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 200 1 1000 0 0
+
+# N = 1000; T = 200; eps = 1; tree_optimized 
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 0 &./bin/test_tree_all_run 2 12345/ bin40_ss2.txt 200 1 1000 2 0
+
+# N = 1000; T = 200; eps = 0.1; leaf  
+./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 200 0.1 1000 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss2.txt 200 0.1 1000 2 1
+
+# N = 1000; T = 200; eps = 0.1; tree_optimized 
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 0.1 1000 2 0 &./bin/test_tree_all_run 2 12345/ bin40_ss2.txt 200 0.1 1000 2 0
+
+# N = 200; T = 200; eps = 1; leaf
+./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 200 1 200 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss2.txt 200 1 200 2 1
+
+# N = 200; T = 1000; eps = 1; tree_optimized
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 0 &./bin/test_tree_all_run 2 12345/ bin40_ss2.txt 200 1 1000 2 0
+
+
