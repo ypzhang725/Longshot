870e88ee953ca62f0b379cd4c995f478036d3971
Yanping <yz488@duke.edu>
2022-08-22 03:55:55 +0000

    re

---
+++ emp-sh2pc/tree_run_all2.sh
@@ -0,0 +1,6 @@
+#four input: datasetName; T; eps; N; sortOption(0: sort subRoot; 2; sortD); outIndex
+# simple dataset for debug
+#./bin/test_tree_all_run 1 12345 ss_1.txt 8 1 10 2 1 &./bin/test_tree_all_run 2 12345 ss_2.txt 8 1 10 2 1
+# NYCTaxi dataset
+#./bin/test_tree_all_run 1 12345 taxi_ss1.txt 10 1 100 2 0 &./bin/test_tree_all_run 2 12345 taxi_ss2.txt 10 1 100 2 0
+./bin/test_tree_all_run 1 12345 bin40_ss1.txt 1000 1 100 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 1000 1 100 2 0
