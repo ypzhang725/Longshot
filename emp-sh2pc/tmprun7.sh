d41949e146205e668b092639ee85f3f3b633c0d9
Yanping <yz488@duke.edu>
2022-08-30 02:44:09 +0000

    ree

--- emp-sh2pc/tmprun7.sh
+++ emp-sh2pc/tmprun7.sh
@@ -1,8 +1,8 @@
 #Tree approach 
 echo "#T=200, eps= 2, N=1000"
 ./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 2 1000 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 200 2 1000 2 0
-echo "#T=200, eps= 1, N=1000"
-./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 200 1 1000 2 0
+#echo "#T=200, eps= 1, N=1000"
+#./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 1 1000 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 200 1 1000 2 0
 echo "#T=200, eps= 0.5, N=1000"
 ./bin/test_tree_all_run 1 12345 bin40_ss1.txt 200 0.5 1000 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 200 0.5 1000 2 0
 echo "#T=200, eps= 0.25, N=1000"
