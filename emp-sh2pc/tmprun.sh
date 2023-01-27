b5b0f26ed2e715c909496253eff32df40b0607e3
yanping <yz488@duke.edu>
2023-01-27 01:28:28 -0500

    cleaned version

--- emp-sh2pc/tmprun.sh
+++
@@ -1,9 +0,0 @@
-start_time=$(date +%s)
-./bin/test_leaf_all_run 1 12345 bin40_ss1.txt 1000 1 1000 2 1 &./bin/test_leaf_all_run 2 12345 bin40_ss1.txt 1000 1 1000 2 1
-end_time1=$(date +%s)
-elapsed1=$(( end_time1 - start_time ))
-echo $elapsed1
-./bin/test_tree_all_run 1 12345 bin40_ss1.txt 1000 1 1000 2 0 &./bin/test_tree_all_run 2 12345 bin40_ss2.txt 1000 1 1000 2 0
-end_time2=$(date +%s)
-elapsed2=$(( end_time2 - start_time ))
-echo $elapsed2
