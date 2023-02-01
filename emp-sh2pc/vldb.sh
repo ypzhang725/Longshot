ba2ab71a03dc02f83aba69f2ceb9f4a2202f1365
Yanping <yz488@duke.edu>
2023-02-01 18:52:07 +0000

    sh wait to syncronize

--- emp-sh2pc/vldb.sh
+++ emp-sh2pc/vldb.sh
@@ -1,22 +1,42 @@
 # N = 1000; T = 200; eps = 1; leaf  
 ./bin/test_leaf_all_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 2 1 0.001 &./bin/test_leaf_all_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 2 1 0.001 
 
+sleep 160 &
+wait $!
+
 # N = 1000; T = 200; eps = 1; tree_naive 
 ./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 0 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 0 0 0.001 
+
+sleep 160 &
+wait $!
 
 # N = 1000; T = 200; eps = 1; tree_optimized 
 ./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 2 0 0.001 
 
+sleep 160 &
+wait $!
+
 # N = 1000; T = 600; eps = 1; tree_optimized 
 ./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 600 1 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 600 1 1000 2 0 0.001 
 
+sleep 160 &
+wait $!
+
 #Tree approach 
 echo "#T=200, eps= 2, N=1000"
-./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 2 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 2 1000 2 0 0.001 
+./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 2 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 2 1000 2 0 0.001
+
+sleep 160 &
+wait $!
+
 #echo "#T=200, eps= 1, N=1000"
 #./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 2 0 0.001 
 echo "#T=200, eps= 0.5, N=1000"
-./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 0.5 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 0.5 1000 2 0 0.001 
+./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 0.5 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 0.5 1000 2 0 0.001
+
+sleep 160 &
+wait $!
+
 echo "#T=200, eps= 0.25, N=1000"
 ./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 0.25 1000 2 0 0.001 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 0.25 1000 2 0 0.001 
 #echo "#T=200, eps= 0.125, N=1000"
@@ -26,5 +46,10 @@ echo "#T=200, eps= 0.25, N=1000"
 #./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 2 0 0.007 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 2 0 0.007
 #./bin/test_tree_all_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 2 0 0.01 &./bin/test_tree_all_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 2 0 0.01 
 
+sleep 160 &
+wait $!
 
 ./bin/test_leaf_baseline_run 1 12345 bin40_total_amount_ss1.txt 200 1 1000 2 1 0.001 &./bin/test_leaf_baseline_run 2 12345 bin40_total_amount_ss2.txt 200 1 1000 2 1 0.001 
+
+
+wait
